fe871bd3b696b5280db587e90d8777a87229361a
Gareth Robinson <gazz_1998@hotmail.co.uk>
2022-01-28 14:11:31 +0000

    basic clock function created with hrs mins and sec

---
+++ Javascript.js
@@ -0,0 +1,8 @@
+//creating the time function
+function time() {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    let meridian = "AM";
+}
